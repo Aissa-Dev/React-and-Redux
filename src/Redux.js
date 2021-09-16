16d3015657ae216e5811f21b20fab7046d721a59
aissamihi <aissamihi@gmail.com>
2021-09-15 21:32:41 -0400

    Redux : Actions, Reducer, Store, Dispatcher

---
+++ src/Redux.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { createStore } from "redux";
+
+function Redux() {
+  // Actions
+  const increment = () => {
+    return {
+      type: "INCREMENT"
+    };
+  };
+
+  const decrement = () => {
+    return {
+      type: "DECREMENT"
+    };
+  };
+
+  // Reducer
+  const counter = (state = 0, action) => {
+    switch (action.type) {
+      case "INCREMENT":
+        return state + 1;
+      case "DECREMENT":
+        return state - 1;
+    }
+  };
+
+  // Store
+  const store = createStore(counter);
+
+  store.subscribe(() => console.log(store.getState()));
+
+  // Dipatcher
+  store.dispatch(increment());
+  store.dispatch(decrement());
+
+  return <div></div>;
+}
+
+export default Redux;
